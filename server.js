9e238b7ba50bb47da3b53c0d3f7a8bf69d18d245
Nandan Shivanna <nandansrivatsa4@gmaol.com>
2026-09-07 19:27:10 +0530

    Fix CORS to allow ngrok-skip-browser-warning header

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"],
   })
 );
 
